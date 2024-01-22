34c12835e9df6b37854d1c8636e1e88a49592439
yukieiji <yuki31gakki97@yahoo.co.jp>
2024-01-22 22:42:43 +0900

    feat:beta texts

--- BetaContentManager.cs
+++ BetaContentManager.cs
@@ -1,5 +1,35 @@
-﻿namespace ExtremeRoles.Beta;
+﻿using SemanticVersioning;
+
+using Il2CppCollection = Il2CppSystem.Collections.Generic;
+
+namespace ExtremeRoles.Beta;
 
 public class BetaContentManager
 {
+	public const string Version = "1.0.0";
+
+	public const string NewTransDataPath = "ExtremeRoles.Beta.Resources.JsonData.TextRevamp.json";
+
+	public const string TransKey = "PublicBetaContent";
+
+	// ここでベータモードで有効になっている物一覧を追加する
+	public static void AddContentText(
+		SupportedLangs curLang,
+		Il2CppCollection.Dictionary<string, string> transData)
+	{
+		string content = curLang switch
+		{
+			SupportedLangs.Japanese =>
+				"・役職説明のテキストの改善/変更\n・フィードバックシステムの追加",
+			_ => "",
+		};
+		transData.Add(TransKey, content);
+
+		// フィードバックを送るよう
+		transData.Add("SendFeedBackToExR", curLang switch
+		{
+			SupportedLangs.Japanese => "フィードバックを送る",
+			_ => "",
+		});
+	}
 }
